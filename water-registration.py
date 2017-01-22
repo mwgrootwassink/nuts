2d6bec930d4fb4772e9d448284ffdfc69644fa64
Maarten Groot Wassink <github@grootwassink.nl>
2017-01-22 23:30:21 +0100

    update

--- water-registration.py
+++ water-registration.py
@@ -1,8 +1,11 @@
 import RPi.GPIO as GPIO
 from datetime import datetime
+import shutil
 
 # init
 data_dir = '/data/nuts'
+data_dir_ready_to_send = '/data/nuts/ready_to_send'
+
 json_template = '{"registrationdate":"%s","value":"%s"}'
 board_mode = GPIO.BOARD
 board_pin = 40
@@ -16,8 +19,11 @@ def my_callback(channel):
     value = GPIO.input(board_pin)
     current_datetime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")    
     fName = data_dir+'/'+current_datetime[0:4]+current_datetime[5:7]+current_datetime[8:10]+current_datetime[11:13]+current_datetime[14:16]+current_datetime[17:19]
+    fNameReadyToSend = data_dir_ready_to_send+'/'+current_datetime[0:4]+current_datetime[5:7]+current_datetime[8:10]+current_datetime[11:13]+current_datetime[14:16]+current_datetime[17:19]
     with open(fName, 'w') as f:
         f.write(format(json_template % (current_datetime, value)))
+    f.close
+    shutil.move(fName, fNameReadyToSend)    
 
 #main
 GPIO.add_event_callback(board_pin, my_callback)
