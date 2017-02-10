40c689be07f463b58cae7daa21bb05494b037a12
Maarten Groot Wassink <github@grootwassink.nl>
2017-02-10 22:43:07 +0100

    Bug with edge detecting solved

--- water-registration.py
+++ water-registration.py
@@ -13,13 +13,13 @@ board_mode = GPIO.BOARD
 board_pin = 40
 
 today = datetime.now().strftime("%Y-%m-%d")
-units = 0
+total_day_units = 0
 unit_description = "Halve liter" 
 
 #setup
 GPIO.setmode(board_mode)
-GPIO.setup(board_pin, GPIO.IN)
-GPIO.add_event_detect(board_pin, GPIO.BOTH)
+GPIO.setup(board_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.add_event_detect(board_pin, GPIO.BOTH, bouncetime=500)
 
 def log(message):
     logtime_datetime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")    
@@ -27,33 +27,42 @@ def log(message):
         logfile.write(logtime_datetime+" - "+message+"\n")
     logfile.close()
 
-def my_callback(channel):
-    global units
+def process_event():
+    event_timestamp = str(datetime.now())
+    event_timestamp_short = event_timestamp[0:19]
+    event_timestamp_supershort = event_timestamp[0:4]+event_timestamp[5:7]+event_timestamp[8:10]+event_timestamp[11:13]+event_timestamp[14:16]+event_timestamp[17:19]
+    time.sleep(1)
     value = GPIO.input(board_pin)
-    current_datetime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")    
-    fName = data_dir+'/'+current_datetime[0:4]+current_datetime[5:7]+current_datetime[8:10]+current_datetime[11:13]+current_datetime[14:16]+current_datetime[17:19]
-    fNameReadyToSend = data_dir_ready_to_send+'/'+current_datetime[0:4]+current_datetime[5:7]+current_datetime[8:10]+current_datetime[11:13]+current_datetime[14:16]+current_datetime[17:19]
+    print(event_timestamp+" - DO-waarde:"+str(value)+" totaal units:"+str(total_day_units))
+    fName = data_dir+'/'+ event_timestamp_supershort
+    fNameReadyToSend = data_dir_ready_to_send+'/'+event_timestamp_supershort
     with open(fName, 'w') as f:
-        f.write(format(json_template % (current_datetime, value)))
+        f.write(format(json_template % (event_timestamp_short, value)))
     f.close()
     shutil.move(fName, fNameReadyToSend)
-    units = units+1
-    log(unit_description+" gebruikt")
+    log(unit_description+" gebruikt DigitalOut-waarde:"+str(value))
 
 #main
 log("Start water-registration.py")
 
-GPIO.add_event_callback(board_pin, my_callback)
+while (True):
 
-while (1 == 1):
-    # At day-change report total units used
+    # Process detected event
+    if (GPIO.event_detected(board_pin)):
+        GPIO.remove_event_detect(board_pin)
+        total_day_units = total_day_units+1
+        process_event()
+        GPIO.add_event_detect(board_pin, GPIO.BOTH, bouncetime=500)
+         
+    # At day-change report total_day_units
     current_day = datetime.now().strftime("%Y-%m-%d")
     if (current_day != today):
-        log("Totaal gebruik "+str(today)+": "+str(units)+" ("+unit_description+")")
+        log("Totaal gebruik "+str(today)+": "+str(total_day_units)+" ("+unit_description+")")
         today = current_day
-        units = 0
+        total_day_units = 0
    
     #Give CPU back
-    time.sleep(1)
+    time.sleep(0.10)
+    
 
 log("End water-registration.py")
